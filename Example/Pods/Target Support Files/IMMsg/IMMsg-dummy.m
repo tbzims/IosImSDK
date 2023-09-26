e580e6df4fda28c8ad5b437cb9323ddcf6d79b9e
oceanMAC <xudehuai@SN-0126.local>
2023-09-26 17:58:54 +0800

    first commit

---
+++ Example/Pods/Target Support Files/IMMsg/IMMsg-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IMMsg : NSObject
+@end
+@implementation PodsDummy_IMMsg
+@end
---
+++ Example/Pods/Target Support Files/Pods-IMMsg_Tests/Pods-IMMsg_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IMMsg_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_IMMsg_Tests
+@end
